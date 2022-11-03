8783f517f8d5a39eb79f217077e11fd235261c7b
Emerson Diniz Leme <83693726+EmersonDJob@users.noreply.github.com>
2022-11-03 08:42:00 -0300

    Update mongodb.js

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function startDB() {
-    await mongoose.connect('mongodb+srv://emersondll:Salmo70@cluster0.elepoxk.mongodb.net/test');
+    await mongoose.connect('mongodb+srv://');
 }
 
 
